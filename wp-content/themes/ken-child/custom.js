d1dc7f0c01c932a79b253ee3027b984df4a91113
berniemcgrail <berniemcgrail@gmail.com>
2015-06-21 23:11:04 +0100

    Homepage

--- wp-content/themes/ken-child/custom.js
+++ wp-content/themes/ken-child/custom.js
@@ -2,9 +2,20 @@
 
     "use strict";
 
-    /*
-		Your custom JS
-	*/
+    var $header = $('header');
+    var $ham = $('.hamburger');
 
+    function navSlide(){
+    	
+    	if($header.hasClass('active')){
+    		$header.removeClass('active');
+    	}else{
+    		$header.addClass('active');
+    	}
+    }
+    
+    $ham.click(function(){
+		navSlide();
+	});
 
 })(jQuery);
